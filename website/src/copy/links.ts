d1299b55ec7c7133d88d118c23abcc9edeed2a3e
Mihatsu Asagiri <267270939+asagirimihatsu@users.noreply.github.com>
2026-09-15 15:11:04 +0900

    Transfer repo to org

--- website/src/copy/links.ts
+++ website/src/copy/links.ts
@@ -1,9 +1,9 @@
 export const LINKS = {
   marketplace: 'https://marketplace.visualstudio.com/items?itemName=asagirimihatsu.jpnov',
-  github: 'https://github.com/asagirimihatsu/vscode-jpnov',
-  readmeJa: 'https://github.com/asagirimihatsu/vscode-jpnov#readme',
-  readmeEn: 'https://github.com/asagirimihatsu/vscode-jpnov/blob/main/README.en.md',
-  license: 'https://github.com/asagirimihatsu/vscode-jpnov/blob/main/LICENSE',
+  github: 'https://github.com/japanese-novel/vscode-jpnov',
+  readmeJa: 'https://github.com/japanese-novel/vscode-jpnov#readme',
+  readmeEn: 'https://github.com/japanese-novel/vscode-jpnov/blob/main/README.en.md',
+  license: 'https://github.com/japanese-novel/vscode-jpnov/blob/main/LICENSE',
   vscode: 'https://code.visualstudio.com/',
   aozora: 'https://www.aozora.gr.jp/annotation/index.html',
   aozoraNeko: 'https://www.aozora.gr.jp/cards/000148/card789.html',
